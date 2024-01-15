82f13ee8c00eeb706bc56b00ec762dd99b31b7f4
tanvir07-cell <tanvir15-14402@diu.edu.bd>
2024-01-15 20:57:10 +0600

    Chore:for the split_streams

---
+++ streams/split2_streams/split.js
@@ -0,0 +1,40 @@
+// by default split2 uses \n as the delimiter
+
+
+// const split = require("split2")
+// const through = require("through2")
+// let lineCount = 0;
+// process.stdin
+// .pipe(split())
+// .pipe(through(write,end))
+// function write(buffer,encoding,next){
+//     lineCount++;
+//     next()
+// }
+// function end(done){
+//     console.log(lineCount)
+//     done()
+// }
+
+
+
+// if i count the line with space then i have to use this code
+
+const split = require("split2")
+const through = require("through2")
+let spaceCount = 0;
+
+process.stdin
+.pipe(split(/\s+/))
+.pipe(through(write,end))
+
+function write(buf,enc,next){
+    spaceCount++;
+    next()
+
+}
+
+function end(done){
+    console.log(spaceCount);
+    done()
+}
